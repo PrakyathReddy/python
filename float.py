36b9a0dbc3e90b3652e1f2098960f97f24baf9c4
PrakyathReddy <prakyathreddyk@gmail.com>
2025-09-26 14:44:26 +0530

    floating integers

---
+++ float.py
@@ -0,0 +1,17 @@
+n1 = 5
+n2 = 12
+
+add = n1+n2
+print("addition: ", add)
+
+sub = n2 - n1
+print("subtraction: ", sub)
+
+mul = n1*n2
+print("multiplication: ", mul)
+
+div = n2/n1
+print("division: ", div)
+
+round = round(div,2)
+print("rounded up value:", round)
